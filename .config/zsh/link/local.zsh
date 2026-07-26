08fe62a107c96c73383fba6d0701007cac26a714
Miles Duncan <milesduncan23@gmail.com>
2026-07-26 17:09:53 -0400

    ssh config update

--- .config/zsh/link/local.zsh
+++ .config/zsh/link/local.zsh
@@ -2,3 +2,16 @@
 export PATH="/opt/nvim-linux-x86_64/bin:$PATH"
 export EDITOR=/opt/nvim-linux-x86_64/bin/nvim
 export VISUAL=/opt/nvim-linux-x86_64/bin/nvim
+
+# Vaultwarden
+alias vw='sudo docker compose -f /srv/vaultwarden/compose.yaml'
+
+vw-on() (
+        sudo systemctl start docker.service
+        vw up -d
+)
+
+vw-off() (
+        vw down
+        sudo systemctl stop docker.socket docker.service containerd.service
+)
